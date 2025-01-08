0703b9ded7d07563148a1ae050c801f64249ebe9
utsav1213 <hiraprautsav54@gmail.com>
2025-01-08 10:04:09 +0530

    stage-2 task has been added

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-import ollama from "ollama";
+// import ollama from "ollama";
 
-async function runChat() {
+// async function runChat() {
+//   try {
+//     const response = await ollama.chat({
+//       model: "llama3.2:3b",
+//       messages: [{ role: 'user', content: "Create API documentation" }]
+//     });
+
+//     console.log("Chatbot Response:", response.message.content);
+//   } catch (error) {
+//     console.error("Error occurred:", error.message);
+//   }
+// }
+
+// runChat();
+//---------------------------------------------------------------------------------
+//  Stage:2
+//---------------------------------------------------------------------------------
+
+import ollama from "ollama";
+import fs from "fs";
+
+let q= fs.readFileSync("./q.txt", "utf-8");
+console.log(q)
+
+askQuestion()
+async function askQuestion() {
   try {
     const response = await ollama.chat({
       model: "llama3.2:3b",
-      messages: [{ role: 'user', content: "Create API documentation" }]
+      messages: [{ role: 'user', content: q }]
     });
 
-    console.log("Chatbot Response:", response.message.content);
+    fs.writeFileSync("./a.txt", response.message.content);
+
   } catch (error) {
     console.error("Error occurred:", error.message);
   }
 }
-
-runChat();
